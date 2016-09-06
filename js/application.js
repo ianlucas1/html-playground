44373cf7f010922753940bd210d4dc91c97e14e5
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-09-06 15:49:35 -0700

    Adds jquery confirm and dynamic link adder

--- js/application.js
+++ js/application.js
@@ -1,3 +1,70 @@
-$(functiuon){
-  console.log("Made it");
-}
+$(document).ready(function() {
+       
+  $('body').on('click', 'li a', function(event){       
+    event.preventDefault();
+
+    var URL = $(this).attr("href");
+    var prompt = "Do you really want to go to " + $(this).text() + "?";
+    var userConfirmation = confirm(prompt);
+    
+    if (userConfirmation)
+      window.location = URL;
+  });
+
+
+  $('body').on('click', '.add-link', function(event){       
+    event.preventDefault();
+
+    var li = $("<li>")
+    var a = $("<a href='http://twitter.com'>Twitter</a>");
+    
+    li.append(a);
+
+    $("ul").append(li);
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// shorthand for document ready
+$(function(){
+  // application code goes here
+});
